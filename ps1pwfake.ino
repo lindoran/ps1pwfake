51b2ed4957808b03154ae3664f3761050d1e8070
lindoran <lindoranster@gmail.com>
2024-10-02 22:24:36 -0500

    update comments

--- ps1pwfake.ino
+++ ps1pwfake.ino
@@ -18,26 +18,27 @@ PIN GND-> GND from ATX, and other leg of the switch.
 
 on a bare attiny85/45/25/13
 
+on a bare attiny85/45/25/13
+
 PIN 8 -> SDBY_PWR (+5V)
 PIN 7 -> one leg of the power switch
 PIN 6 -> PWR_ON on atx Supply
 PIN 4 -> GND from ATX, and the other leg of the switch.
-
-if uploading from a eeprom programmer for attiny85 use check the following fuses:
-low byte: E2
-SUT0=0, cksel3=0, cksel2=0, cksel0=0
-high byte: DF
-SPIEN=0
-
-leave the rest unchecked (ext. and lock bit should be FF)
 
 If after programming you have +5V on pin 7 when applying 5v to
 pin 8 and ground to pin 4, the programming was successful.
 
 I recommend cutting the extra legs off splaying the chip out flat and soldering
 wires directly to the chips pin, then incase the chip in a 1/2 inch heat
-shrink tubing.
+shrink tubing.                                                                                    
 
+if uploading from a mini-pro programmer for attiny85 use check the following in fuses:
+low byte: E2
+SUT0=0, cksel3=0, cksel2=0, cksel0=0
+high byte: DF
+SPIEN=0
+
+leave the rest unchecked (ext. and lock bit should be FF)
 see screenshot in attiny85 folder.
 
 
@@ -81,11 +82,11 @@ DEALINGS IN THE SOFTWARE.
 
 
 
-#define       PWR_SW             2     // port B2, pin10 on ml-tiny
-#define       PWR_ON             1     // port B1, pin9 on ml-tiny, other side of switch to GND
+#define       PWR_SW             2     // port B2, pin10 on ml-tiny, pin 7 on attiny85
+#define       PWR_ON             1     // port B1, pin9 on ml-tiny, pin 6 on attiny85 
 
-// power supply standby_pwr to VCC / +5v  
-// power supply GND to GND
+// power supply standby_pwr to VCC / +5v (pin 8 on attiny85) 
+// power supply GND and 2nd switch leg to GND (pin 4 on attiny85) 
 
 // caution! see warning about programer voltages above.
 
